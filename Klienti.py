13fd6f3fc009b5d5bb4827c754d6a478dc63a4f7
dielsamaloku3 <dielsa.maloku@student.uni-pr.edu>
2026-05-09 11:57:54 +0200

    ndryshime

--- Klienti.py
+++ Klienti.py
@@ -6,27 +6,24 @@ from Crypto.Hash import SHA256
 
 
 def start_client():
-    # 1. Lexo celesin privat te klientit per te nenshkruar mesazhin
     with open("client_private.pem", "rb") as f:
         client_private_key = RSA.import_key(f.read())
 
-    # 2. Lexo certifikaten e serverit (X.509) per te kriptuar mesazhin
+  
     with open("server_cert.crt", "rb") as f:
         server_public_key = RSA.import_key(f.read())
     cipher_rsa = PKCS1_OAEP.new(server_public_key)
 
-    # Merr mesazhin nga tastiera (perdoruesi e shkruan vete)
+    
     user_input = input("Shkruaj mesazhin qe deshiron te dergosh te Serveri: ")
     message = user_input.encode()
 
-    # 3. Krijimi i Nenshkrimit Digjital (Digital Signature)
+  
     h = SHA256.new(message)
     signature = pkcs1_15.new(client_private_key).sign(h)
 
-    # 4. Kriptimi i mesazhit
     encrypted_msg = cipher_rsa.encrypt(message)
 
-    # 5. Lidhja TCP dhe dergimi
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(("localhost", 12345))
 
