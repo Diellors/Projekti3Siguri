7153db5c8a66cba1ad8bd942f6fc76e32215f9ba
dielsamaloku3 <dielsa.maloku@student.uni-pr.edu>
2026-05-09 11:46:17 +0200

    Shto funksionin start_client

--- Klienti.py
+++ Klienti.py
@@ -3,3 +3,11 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
+
+
+def start_client():
+    pass
+
+
+if __name__ == "__main__":
+    start_client()
